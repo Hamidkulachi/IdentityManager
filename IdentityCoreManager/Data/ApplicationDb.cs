ce6048bc066915409b4d7e722cbd519ad6ce5076
Hamid eng <hamiddikhan@gmail.com>
2023-10-02 07:38:33 -0700

    Add project files.

---
+++ IdentityCoreManager/Data/ApplicationDb.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityCoreManager.Data
+{
+    public class ApplicationDb:IdentityDbContext
+    {
+        public ApplicationDb(DbContextOptions options):base(options)
+        {
+                
+        }
+    }
+}
